c610fbb47521c6930c3a2d3189370733be776669
mario mora <jorgemariomoramolina@gmail.com>
2026-01-23 16:56:55 -0500

    products module

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,23 +1,9 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Param,
-  Post,
-  Body,
-  Put,
-  Delete,
-  HttpStatus,
-  HttpCode,
-  Res,
-  // ParseIntPipe,
-} from '@nestjs/common';
-
+import {Controller, Get, Query, Param, Post, Body, Put, Delete, HttpStatus, HttpCode, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { ParseIntPipe } from '../common/parse-int.pipe';
-import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
+import { ParseIntPipe } from '../../common/parse-int.pipe';
+import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
 
-import { ProductsService } from './../services/products.service';
+import { ProductsService } from '../services/products.service';
 
 @Controller('products')
 export class ProductsController {
--- src/products/dtos/products.dtos.ts
+++ src/products/dtos/products.dtos.ts
@@ -1,10 +1,4 @@
-import {
-  IsString,
-  IsNumber,
-  IsUrl,
-  IsNotEmpty,
-  IsPositive,
-} from 'class-validator';
+import { IsString, IsNumber, IsUrl, IsNotEmpty, IsPositive } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateProductDto {
