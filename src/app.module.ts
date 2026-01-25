242c887d6fe05146e2086699feafedf890028cff
mario mora <jorgemariomoramolina@gmail.com>
2026-01-24 22:57:54 -0500

    useValue and useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/controllers/products.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '1234567890';
+const API_KEY2 = '0987654321';
+
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY2 : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
