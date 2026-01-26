1f39842cc0445e8416a4630bfd218aec79880358
mario mora <jorgemariomoramolina@gmail.com>
2026-01-26 16:13:37 -0500

    validating to enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module, HttpModule, HttpService } from '@nestjs/common';
+import config from './config';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
-import config from './config';
 
 @Module({
   imports: [
@@ -14,6 +16,11 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        API_KEY: Joi.string().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
