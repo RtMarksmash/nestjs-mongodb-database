15c82a33c7655c2a2d0acca1b64ca4e31ad82b12
mario mora <jorgemariomoramolina@gmail.com>
2026-02-02 21:57:26 -0500

    database conection enviroment variables

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,8 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
+
+import config from 'src/config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
@@ -17,13 +20,16 @@ const API_KEY_PROD = 'PROD1212121SA';
     },
     {
       provide: 'MONGO',
-      useFactory: async () => {
-        const uri = `mongodb://root:root@localhost:27017/?authSource=admin`;
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { user, password, port, host, dbName, connection } =
+          configService.mongo;
+        const uri = `${connection}://${user}:${password}@${host}:${port}/?authSource=admin`;
         const client = new MongoClient(uri);
         await client.connect();
-        const database = client.db('platzi-store');
+        const database = client.db(dbName);
         return database;
       },
+      inject: [config.KEY],
     },
   ],
   exports: ['API_KEY', 'MONGO'],
